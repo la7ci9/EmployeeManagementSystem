6ceaba68985fe1d21aa463edcce4f1b56172a254
la7ci9 <akarmiis997@gmail.com>
2022-07-13 18:12:52 +0200

    Added anotations

--- src/main/java/com/learn/controller/EmployeeController.java
+++ src/main/java/com/learn/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.learn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import com.learn.service.EmployeeService;
 @Controller
 public class EmployeeController {
 
+	@Autowired
 	private EmployeeService employeeService;
 
 	public EmployeeController(EmployeeService employeeService) {
--- src/main/java/com/learn/entity/Employee.java
+++ src/main/java/com/learn/entity/Employee.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Employees")
+@Table(name = "employees")
 public class Employee {
 	
 	@Id
