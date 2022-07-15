1048ce2dd193cea7eada4e50517528376339d999
la7ci9 <akarmiis997@gmail.com>
2022-07-15 15:35:08 +0200

    implement methods

--- src/main/java/com/learn/controller/EmployeeController.java
+++ src/main/java/com/learn/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.learn.entity.Employee;
@@ -36,5 +37,33 @@ public class EmployeeController {
 	public String saveEmployee(@ModelAttribute("employee") Employee emp) {
 		employeeService.saveEmployee(emp);
 		return "redirect:employees";
+	}
+	
+	@GetMapping("/employees/edit/{id}")
+	public String editStudent(@PathVariable Long id, Model model) {
+		model.addAttribute("employee",employeeService.getEmployeeById(id));
+		return "update_employee";
+	}
+	
+	//if you press the button in edit 
+	@PostMapping("/employees/{id}")
+	public String updateStudent(@PathVariable Long id,
+			@ModelAttribute("employee") Employee emp, Model model) {
+		
+		Employee existingStudent = employeeService.getEmployeeById(id);
+		existingStudent.setId(id);
+		existingStudent.setFirstName(emp.getFirstName());
+		existingStudent.setLastName(emp.getLastName());
+		existingStudent.setPhoneNumber(emp.getPhoneNumber());
+		existingStudent.setEmail(emp.getEmail());
+		existingStudent.setPosition(emp.getPosition());
+		
+		employeeService.saveEmployee(existingStudent);
+		return "redirect:/employees";
+	}
+	@GetMapping("/employees/{id}")
+	public String deleteEmployee(@PathVariable Long id) {
+		employeeService.deleteEmployeeById(id);
+		return "redirect:/employees";
 	}
 }
